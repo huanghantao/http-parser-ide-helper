3513ddf7a2fb5f6bc9e7b75cb1b66f7dd14c5136
codinghuang <2812240764@qq.com>
2020-08-21 15:00:20 +0800

    init

---
+++ output/http_parser/namespace/Http/Request/Parser.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Http\Request;
+
+class Parser
+{
+
+    public function __construct()
+    {
+    }
+
+    /**
+     * @return int
+     */
+    public function parse(string $data)
+    {
+    }
+
+    /**
+     * @return string
+     */
+    public function getMethod()
+    {
+    }
+
+    /**
+     * @return string
+     */
+    public function getURL()
+    {
+    }
+
+    /**
+     * @return int
+     */
+    public function getVersion()
+    {
+    }
+
+    /**
+     * @return array
+     */
+    public function getHeaders()
+    {
+    }
+
+    /**
+     * @return string
+     */
+    public function getBody()
+    {
+    }
+}
